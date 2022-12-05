596a9c5dde4fd9f26f44eb0a35784561edb495aa
DELL <DELL@DESKTOP-59FMM7T>
2022-12-05 17:56:53 +0530

    modification

---
+++ src/test/java/jenkins/Testcase5.java
@@ -0,0 +1,12 @@
+package jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+@Test
+public class Testcase5 {
+@Test
+	public void tc5()
+	{
+		Reporter.log("Hello Python",true);
+	}
+}
